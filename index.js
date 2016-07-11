d9782b26a05d6c9c64b3477fcad8ae8826cc5df9
Oleg verhovsky <verchol@gmail.com>
2016-07-11 18:17:16 +0300

    add FILE_TO_WATCH

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var serviceDiscover = require('serviceDiscovery');
-serviceDiscover.watch("./tmp/container-map",(err, data)=>{
+serviceDiscover.watch(process.env.FILE_TO_WATCH, (err, data)=>{
   if (err){
    console.log('failed')
    return;
