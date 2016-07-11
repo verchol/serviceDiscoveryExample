c789e2fb7af2de56db5e2119e33a843d3bcbda3d
Oleg verhovsky <verchol@gmail.com>
2016-07-11 20:07:28 +0300

    fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var serviceDiscover = require('serviceDiscovery');
- require('./bin/www');
+
 serviceDiscover.watch(process.env.FILE_TO_WATCH, (err, data)=>{
   if (err){
    console.log('failed')
    return;
 }
  console.log('service discovery file detected!');
-// console.log('model is : ' + JSON.stringify(serviceDiscover.model()));
-
+ console.log('model is : ' + JSON.stringify(serviceDiscover.model()));
+ require('./bin/www');
  //serviceDiscover.unwatch();
 
 });
